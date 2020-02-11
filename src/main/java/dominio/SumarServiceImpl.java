4858cdc386d3a4f30942b813a0a9b4ff659f08f6
--list <agustincammarota@hotmail.com>
2020-02-11 19:59:09 -0300

    probando commit

--- src/main/java/dominio/SumarServiceImpl.java
+++ src/main/java/dominio/SumarServiceImpl.java
@@ -8,7 +8,7 @@ import javax.jws.WebService;
 //Indicamos que es un web Service y que se implementa de la interfaz
 @WebService(endpointInterface = "beans.SumarDaoService")
 public class SumarServiceImpl implements SumarDaoService {
-
+// prueba
     public int sumar(int a, int b) {
         return a + b;
     }
